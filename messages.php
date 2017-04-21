257edc5454fc8a0edc4ff2befb4f4f16746436fd
thompson filgueiras <thompson filgueiras>
2017-04-21 16:56:22 -0300

    messages_array

--- messages.php
+++ messages.php
@@ -4,11 +4,37 @@
     $alert   = (isset($alert))  ?$alert  :"";
     $success = (isset($success))?$success:"";
     $erro    = (isset($erro))   ?$erro   :"";
+    function createMessageDiv($success, $class){
+        if(is_array($success)){
+            foreach($success as $s){
+                if($s == ""){continue;}
+                ?> 
+                    <div class="alert alert-<?php echo $class; ?> fade in" role="alert">
+                        <div class="content-msg"><?php echo $s; ?></div>
+                        <button type="button" class="close" data-dismiss="alert">
+                            <span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+                        </button>
+                    </div>
+                <?php 
+            }
+        }else{
+            ?> 
+                <div class="alert alert-<?php echo $class; ?> fade in <?php if($success === ""){echo "hidden";}?>" role="alert">
+                    <div class="content-msg"><?php echo $success; ?></div>
+                    <button type="button" class="close" data-dismiss="alert">
+                        <span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+                    </button>
+                </div>
+            <?php 
+        }
+    }
 ?><div id='_messages_container'>
-    <div class="alert alert-success fade in <?php if($success === ""){echo "hidden";}?>" role="alert"><div class="content-msg"><?php echo $success; ?></div><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></div>
-    <div class="alert alert-info fade in    <?php if($info === "")   {echo "hidden";}?>" role="alert"><div class="content-msg"><?php echo $info;    ?></div><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></div>
-    <div class="alert alert-warning fade in <?php if($alert === "")  {echo "hidden";}?>" role="alert"><div class="content-msg"><?php echo $alert;   ?></div><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></div>
-    <div class="alert alert-danger  fade in <?php if($erro === "")   {echo "hidden";}?>" role="alert"><div class="content-msg"><?php echo $erro;    ?></div><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button></div>
+    <?php 
+        createMessageDiv($success, 'success');
+        createMessageDiv($info, 'info');
+        createMessageDiv($alert, 'warning');
+        createMessageDiv($erro, 'danger');
+    ?>
 </div><?php 
 $this->Html->LoadJs('lib/notificacao/messages');
 //$this->Html->LoadJqueryFunction('message_success("Uma falha aconteceu!");');
